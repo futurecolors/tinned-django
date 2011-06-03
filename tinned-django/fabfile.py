77c62f8f5b6171b52576133776cc4b97386231fb
Ilya Baryshev <baryshev@gmail.com>
2011-06-03 18:23:48 +0400

    Установка mysql при full_deploy Подключение альтернативных источников для uwsgi и nginx

--- tinned-django/fabfile.py
+++ tinned-django/fabfile.py
@@ -28,12 +28,44 @@ def prod():
 
     
 def full_deploy():
+    #TODO: install vim, mc
+    mysql.mysql_install()
+    mysql.mysql_create_db()
     deploy.full_deploy()
     chmod.set_chmod()
     dj_cmd.collectstatic()
     dj_cmd.compress()
 
 
+def prepare_server():
+    """ Prepares server: installs system packages. """
+    os = utils.detect_os()
+    if os in ['lenny', 'squeeze']:
+        install_sudo()
+
+    setup_backports()
+    fc_setup_testing_sources()
+    install_common_software()
+
+    
+@utils.run_as('root')
+def fc_setup_testing_sources():
+    """ Adds testing repo to apt sources. """
+    os = utils.detect_os()
+    debian_repo = 'http://mirror.yandex.ru'
+    testing = {
+        'squeeze': '{0}/debian wheezy main non-free contrib'.format(debian_repo),
+    }
+
+    if os not in testing:
+        fabric_utils.puts("Testing sources are not available for " + os)
+        return
+
+    run("echo 'deb %s' > /etc/apt/sources.list.d/wheezy.list" % testing[os])
+    with settings(warn_only=True):
+        run('aptitude update')
+
+        
 def update_django_config(restart=True):
     files.upload_template(
         utils._project_path(env.conf.REMOTE_CONFIG_TEMPLATE),
