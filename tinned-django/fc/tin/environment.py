c12d208e75abcfc1f94c3d7395997dd0488662d6
Ilya Baryshev <baryshev@gmail.com>
2011-06-14 11:15:07 +0400

    Убрана логика назначения портов (заменено на сокеты)

--- tinned-django/fc/tin/environment.py
+++ tinned-django/fc/tin/environment.py
@@ -4,7 +4,6 @@ from fabric.operations import local, prompt
 from fabric.context_managers import lcd, settings, hide
 import os
 import socket
-import sqlite3
 from fc.tin.config_writer import write_template
 from fc.tin.util import usernames_unixgroup
 
@@ -14,41 +13,12 @@ PROJECTS = []
 
 def setup_environment():
 
-    def _connect_sqlite():
-        return sqlite3.connect('/etc/port_mappings/db')
-
-    
-    def _get_max_port():
-        conn = _connect_sqlite()
-        try:
-            r = conn.execute('SELECT MAX(`port`) FROM mapping')
-            for a in r:
-                max_port = a[0]
-                break
-        except sqlite3.OperationalError:
-            max_port = PORTS[0]
-        conn.close()
-        return max_port
-
-
-    def _assign_port(port, project, developer):
-        conn = _connect_sqlite()
-        conn.execute("create table if not exists mapping(port, project, developer)")
-        data = (port, project, developer)
-        conn.execute("insert into mapping(port, project, developer) values (?, ?, ?)", data)
-        conn.commit()
-        conn.close()
-
-
-    def _get_free_port(project, developer):
-        max_port = _get_max_port()
-        free_port = max_port + 1
-        _assign_port(free_port, project, developer)
-        return free_port
-
-
     def _get_path_to_virtualenv(project, developer):
         return '/home/{developer}/projects/{project}/env/'.format(project=project, developer=developer)
+
+    
+    def get_socket_path(project, developer):
+        return '/tmp/{0}.{1}.sock'.format(project, developer)
 
 
     def _create_project(name):
@@ -67,7 +37,7 @@ def setup_environment():
         local('sudo mv {0} {1}'.format(outfilepath, target_dir))
 
 
-    def _create_uwsgi_config(project, developer, port):
+    def _create_uwsgi_config(project, developer):
         context = {'SOCKET_PATH': get_socket_path(project, developer),
                    'PATH_TO_VIRTUALENV': _get_path_to_virtualenv(project, developer),
                    'PROJECT_PATH': os.path.join('/home', developer, 'projects', project, 'src'),
@@ -79,11 +49,8 @@ def setup_environment():
                            target_dir='/etc/uwsgi/',
                            outfilepath = '/tmp/{0}.{1}.xml'.format(project, developer))
 
-    def get_socket_path(project, developer):
-        return '/tmp/{0}.{1}.sock'.format(project, developer)
 
-
-    def _create_nginx_config(project, developer, port):
+    def _create_nginx_config(project, developer):
         context = {'SOCKET_PATH': get_socket_path(project, developer),
                    'SERVER_IP': socket.gethostbyname('{0}.{1}'.format(project, developer)),
                    'SERVER_NAME': '{0}.{1}'.format(project, developer)}
@@ -99,10 +66,9 @@ def setup_environment():
                 local('sudo mkdir -p projects')
                 with lcd('projects'):
                     for project in PROJECTS:
-                        port = _get_free_port(project, developer_name)
                         _create_project(project)
-                        _create_uwsgi_config(project, developer_name, port)
-                        _create_nginx_config(project, developer_name, port)
+                        _create_uwsgi_config(project, developer_name)
+                        _create_nginx_config(project, developer_name)
                 local('sudo chown -R {user}:{group} /home/{user}/projects/'.format(user=developer_name, group=DEVELOPERS_USERGROUP))
                 local('sudo chmod -R 755 /home/{user}/projects/'.format(user=developer_name))
 
@@ -111,7 +77,5 @@ def setup_environment():
     PROJECTS = prompt('Введите названия проектов через запятую: ', validate=r'[\w\,]+').split(',')
     DEVELOPERS = usernames_unixgroup(DEVELOPERS_USERGROUP)
     local('sudo mkdir -p /etc/uwsgi/')
-    local('sudo mkdir -p /etc/port_mappings/')
-    local('sudo chmod -R 777 /etc/port_mappings/')
     _create_directory_layout(DEVELOPERS)
     local('sudo /etc/init.d/nginx reload')
