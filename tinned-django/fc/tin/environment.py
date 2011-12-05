321f9d1ed6e530850051e314c10d5d60196eba55
Ilya Baryshev <baryshev@gmail.com>
2011-12-05 17:08:36 +0400

    - hardcode to avoid dns error - logfile-chown  #4636

--- tinned-django/fc/tin/environment.py
+++ tinned-django/fc/tin/environment.py
@@ -54,7 +54,7 @@ def setup_environment():
     def _create_nginx_config(project, developer):
         context = {'SOCKET_PATH': get_socket_path(project, developer),
                    'PROJECT_PATH': os.path.join('/home', developer, 'projects', project, 'src'),
-                   'SERVER_IP': socket.gethostbyname('{0}.{1}'.format(project, developer)),
+                   'SERVER_IP': socket.gethostbyname('{0}.{1}.fcdev.ru'.format(project, developer)),
                    'SERVER_NAME': '{0}.{1}'.format(project, developer)}
         _write_root_config(relative_path='configs/nginx.config',
                            context=context,
