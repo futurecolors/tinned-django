b6a6cd5561fab1de27f4896e307762dade14bfb2
Vitaly <olevinsky.v.s@gmail.com>
2012-10-04 15:18:10 +0400

    Changed sorl engine to “ImageMagick”.

--- tinned-django/_settings/applications/thumbnail.py
+++ tinned-django/_settings/applications/thumbnail.py
@@ -6,6 +6,14 @@ THUMBNAIL_DEBUG = False
 THUMBNAIL_DUMMY = False
 THUMBNAIL_UPSCALE = False
 
+#  This engine uses the ImageMagick convert or GraphicsMagic gm convert command. Features:
+#  Easy to install
+#  Produces high quality images
+#  It is pretty fast
+#  Can handle CMYK sources
+#  It is a command line command, that is less than ideal.
+THUMBNAIL_ENGINE = 'sorl.thumbnail.engines.convert_engine.Engine'
+
 # Хранилище для thumbnail
 THUMBNAIL_KVSTORE = 'sorl.thumbnail.kvstores.redis_kvstore.KVStore'
 THUMBNAIL_REDIS_DB = 1
