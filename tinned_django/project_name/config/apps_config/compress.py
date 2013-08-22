7bea8cd9bc4eb8a7e543a98d1aeca83eacd266c7
Артём <golikoffartem@gmail.com>
2013-08-22 18:33:59 +0400

    Update compress.py

--- tinned_django/project_name/config/apps_config/compress.py
+++ tinned_django/project_name/config/apps_config/compress.py
@@ -1,12 +1,22 @@
 # coding: utf-8
 
 
-class CompressSettings(object):
+less_command = 'lessc {infile} {outfile}'
 
+
+class CompressSettings(object):
     COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',
                             'compressor.filters.cssmin.CSSMinFilter']
+    COMPRESS_PRECOMPILERS = (
+        ('text/less', less_command + ' --rootpath=../'),
+    )
+    COMPRESS_CSS_HASHING_METHOD = 'content'
 
 
 class CompressEnabled(CompressSettings):
     COMPRESS_ENABLED = True
     COMPRESS_OFFLINE = True
+
+    COMPRESS_PRECOMPILERS = (
+        ('text/less', less_command),
+    )
