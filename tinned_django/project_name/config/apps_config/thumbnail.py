93b567226507e34f0fab624538a2efece79dd65a
Ilya Baryshev <baryshev@gmail.com>
2013-08-22 14:01:27 +0400

    Updated thumbnail defaults to fix Imagemagick problems and add redis support

--- tinned_django/project_name/config/apps_config/thumbnail.py
+++ tinned_django/project_name/config/apps_config/thumbnail.py
@@ -8,7 +8,8 @@ class ThumbnailSettings(object):
     THUMBNAIL_DUMMY = False
     THUMBNAIL_UPSCALE = False
 
-    #  This engine uses the ImageMagick convert or GraphicsMagic gm convert command.
+    #  This engine uses the ImageMagick convert or GraphicsMagic
+    #  gm convert command.
     #
     #  Features:
     #    * Easy to install
@@ -17,6 +18,15 @@ class ThumbnailSettings(object):
     #    * Can handle CMYK sources
     #  It is a command line command, that is less than ideal.
     THUMBNAIL_ENGINE = 'sorl.thumbnail.engines.convert_engine.Engine'
+    # non-linear for ImageMagick, otherwise gamma will be screwed
+    THUMBNAIL_COLORSPACE = 'sRGB'
 
     THUMBNAIL_KVSTORE = 'sorl.thumbnail.kvstores.redis_kvstore.KVStore'
+    THUMBNAIL_REDIS_UNIX_SOCKET_PATH = '/var/run/redis/{{ project_name}}.sock'
     THUMBNAIL_REDIS_DB = 1
+
+
+class ThumbnailDebug(ThumbnailSettings):
+    """ For local development"""
+    THUMBNAIL_DUMMY = True
+    THUMBNAIL_DUMMY_SOURCE = 'http://lorempixel.com/%(width)s/%(height)s/nature'
