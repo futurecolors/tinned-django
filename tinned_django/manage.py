174d9862242cecdf89c3fd398b93e805e49dea44
Melevir <melevir@gmail.com>
2013-09-23 15:42:10 +0400

    Set up test environment when launching tests.

--- tinned_django/manage.py
+++ tinned_django/manage.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Development")
 
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        os.environ['DJANGO_CONFIGURATION'] = 'Testing'
+
     from configurations.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
